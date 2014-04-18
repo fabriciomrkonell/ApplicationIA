cef06d4bafd807ec60eeb9905953f59e738c3b06
Fabrício Ronchi <fabricio_konell@hotmail.com>
2014-04-18 19:24:52 -0300

    Up

--- src/inteligenciaartificial/Imagem.java
+++ src/inteligenciaartificial/Imagem.java
@@ -2,8 +2,11 @@ package inteligenciaartificial;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.imageio.ImageIO;
 
@@ -23,15 +26,38 @@ public class Imagem {
         return pixels;    
     }
     
-    public void imprimir(byte[][] imagem, String aluno, String caracter, String tipo) {        
+    public void criarCabecalho(String patterns, String inputs, String outputs) throws IOException {                
+        FileWriter arq = new FileWriter("C:\\Users\\Fabrício Ronchi\\Desktop\\InteligenciaArtificial\\numeros.pat");
+        PrintWriter gravarArq = new PrintWriter(arq);                        
+        String cabecalho = 
+                "SNNS pattern definition file V3.2" +
+                "\n" + 
+                "generated at Mon Apr 25 15:58:23 1994" + 
+                "\n" + 
+                "\n" + 
+                "No. of patterns: " + patterns +
+                "\n" + 
+                "No. of input units: " + inputs +
+                "\n" + 
+                "No. of output units: " + outputs +
+                "\n" + 
+                "\n" + 
+                "\n";
+        gravarArq.printf(cabecalho);                
+        arq.close();                                               
+    };
+    
+    public void imprimir(byte[][] imagem, String aluno, String caracter, String tipo) throws IOException {                                 
+        FileWriter arq = new FileWriter("C:\\Users\\Fabrício Ronchi\\Desktop\\InteligenciaArtificial\\numeros.pat", true);
+        PrintWriter gravarArq = new PrintWriter(arq);        
         for (int i = 0; i < imagem.length; i++) {                        
-            for (int j = 0; j < imagem[i].length; j++) {
-                System.out.print(imagem[j][i]);
-                System.out.print(" ");
+            for (int j = 0; j < imagem[i].length; j++) {                                                     
+                gravarArq.printf(imagem[j][i] + " ");                
             }                      
-            System.out.println();            
+            gravarArq.printf("\n");
         }
-        System.out.println("Imagem: " + aluno + "_"  + caracter + "_" + tipo);  
-        System.out.println("");                                                               
+        gravarArq.printf("\n");
+        gravarArq.printf("\n");
+        arq.close();                                               
     }    
 }
--- src/inteligenciaartificial/InteligenciaArtificial.java
+++ src/inteligenciaartificial/InteligenciaArtificial.java
@@ -1,6 +1,7 @@
 package inteligenciaartificial;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class InteligenciaArtificial {
 
@@ -9,18 +10,21 @@ public class InteligenciaArtificial {
         Imagem obj = new Imagem();
                 
         byte[][] pixels;
+        ArrayList<byte[][]> todos = new ArrayList<>();
         String[] alunos = { "fabricio" };
         String[] imagens = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
         String[] tipo = { "1", "2"};
+        
+        obj.criarCabecalho("3", "400", "4");
         
         for (String aluno : alunos) {
             for (String imagen : imagens) {
                 for (String tipo1 : tipo) {
                     pixels = obj.converter(aluno, imagen, tipo1);        
-                   obj.imprimir(pixels, aluno, imagen, tipo1);
+                    obj.imprimir(pixels, aluno, imagen, tipo1);
                 } 
             }
-        }                                        
+        }                  
     }
 }    
     
